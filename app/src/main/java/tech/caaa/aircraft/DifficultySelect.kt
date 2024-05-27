af2c49da4c9364d3d6691c18fd762882bff6d874
Clouder0 <clouder0@outlook.com>
2024-05-27 23:21:48 +0800

    feat: finish homepage, add diff select

--- app/src/main/java/tech/caaa/aircraft/DifficultySelect.kt
+++ app/src/main/java/tech/caaa/aircraft/DifficultySelect.kt
@@ -1,31 +1,20 @@
 package tech.caaa.aircraft
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class DifficultySelect : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_difficulty_select)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val localModeBtn: Button = findViewById(R.id.localModeBtn)
-        localModeBtn.setOnClickListener {
-            Toast.makeText(this, "button clicked!", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, DifficultySelect::class.java)
-            startActivity(intent)
         }
     }
 }
