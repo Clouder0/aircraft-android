1373de279535ded344668577b1795b85b0bbe029
Clouder0 <clouder0@outlook.com>
2024-05-29 18:22:03 +0800

    chore

--- app/src/main/java/tech/caaa/aircraft/common/Collision.kt
+++ app/src/main/java/tech/caaa/aircraft/common/Collision.kt
@@ -4,21 +4,28 @@ import android.graphics.Rect
 
 
 fun getHitbox(x: Double, y: Double, width: Double, height: Double): Rect {
-    return Rect((x - width * 0.5).toInt(), (y - height * 0.5).toInt(),
+    return Rect(
+        (x - width * 0.5).toInt(), (y - height * 0.5).toInt(),
         (x + width * 0.5).toInt(), (y + height * 0.5).toInt()
     )
 }
+
 abstract class BoxedCollidable(private val width: Double, private val height: Double) {
     abstract val x: Double
     abstract val y: Double
     public fun check(other: BoxedCollidable): Boolean {
-        val hitbox = getHitbox(x,y,width,height)
-        val hitboxOthers = getHitbox(other.x,other.y,other.width,other.height)
-        return hitbox.intersects(hitboxOthers.left, hitboxOthers.top, hitboxOthers.right, hitboxOthers.bottom)
+        val hitbox = getHitbox(x, y, width, height)
+        val hitboxOthers = getHitbox(other.x, other.y, other.width, other.height)
+        return hitbox.intersects(
+            hitboxOthers.left,
+            hitboxOthers.top,
+            hitboxOthers.right,
+            hitboxOthers.bottom
+        )
     }
 
     val hitbox: Rect
         get() {
-            return getHitbox(x,y,width,height)
+            return getHitbox(x, y, width, height)
         }
 }
--- app/src/main/java/tech/caaa/aircraft/common/helper.kt
+++ app/src/main/java/tech/caaa/aircraft/common/helper.kt
@@ -1,9 +1,11 @@
 package tech.caaa.aircraft.common
 
 fun <A, B, C, D, E, F, R> ((A, B, C, D, E, F) -> R).curry(a: A): (B, C, D, E, F) -> R =
-    { b: B, c: C, d: D, e:E, f:F -> this(a, b, c, d,e, f) }
+    { b: B, c: C, d: D, e: E, f: F -> this(a, b, c, d, e, f) }
+
 fun <A, B, C, D, E, R> ((A, B, C, D, E) -> R).curry(a: A): (B, C, D, E) -> R =
-    { b: B, c: C, d: D, e:E -> this(a, b, c, d,e) }
+    { b: B, c: C, d: D, e: E -> this(a, b, c, d, e) }
+
 fun <A, B, C, D, R> ((A, B, C, D) -> R).curry(a: A): (B, C, D) -> R =
     { b: B, c: C, d: D -> this(a, b, c, d) }
 
